4e9ca15f36455c47830d7269dc59a73a8c066dde
Ubuntu <ubuntu@ip-172-31-13-215.us-west-2.compute.internal>
2022-11-26 14:04:17 +0000

    clean raz's crap

--- ass_1/shell.c
+++ ass_1/shell.c
@@ -294,76 +294,6 @@ void external_command_execute(Instruction* curr_inst, JobNode jobs[4])
     
 }
 
-//running external commands!
-int ext_com_exec(Instruction* curr_inst, JobNode job_list[4])
-{
-    struct JobNode* job_pointer;
-    char to_exec[MAX_LINE_LENGHT];
-    strcpy(to_exec, curr_inst->raw_instruction);
-    char path[MAX_LINE_LENGHT];
-    getcwd(path, MAX_LINE_LENGHT);
-    pid_t curr_pid;
-    pid_t dead_child;
-
-    // check free job
-    for (int i=0 ;i<4;i++)
-    {
-        if (job_list[i].is_active == false)
-        {
-            job_list[i].pid = curr_pid;
-            strcpy(job_list[i].raw_instruction, to_exec);
-            job_list[i].is_active = true;
-            break;
-        }
-    }
-
-    /***char *args[] = {&to_exec,"-lh", path, NULL};
-    curr_pid = fork();
-
-    //error forking
-    if (curr_pid<0)
-    {
-        printf("error creating child process"); //TODO
-        return 1;
-    }
-    
-    //father process
-    if (curr_pid!=0)
-    {  
-        printf("hw1shell: pid %d started\n", curr_pid);
-        if (curr_inst->is_backround==false)
-        {
-            printf("Father -  I'm waiting for child pid %d to finish\n", curr_pid);
-            int returnStatus;
-            dead_child = waitpid(curr_pid, &returnStatus, 0);
-            //printf("Father - child pid - %d finished!\n", dead_child);
-        }
-        else
-        {
-            //printf("Father - no need to wait, we continue\n");
-            for (int i=0 ;i<4;i++)
-            {
-                if (job_list[i].is_active == false)
-                {
-                    job_list[i].pid = curr_pid;
-                    strcpy(job_list[i].raw_instruction, to_exec);
-                    job_list[i].is_active = true;
-                    break;
-                }
-            }
-        }
-    }
-
-    //child process
-    else
-    {
-        curr_pid=getpid();
-        execv(to_exec, args);
-        //printf("Child - I finished! now I'll die\n"); //shouldn't be seen
-    }
-    return 0;****/
-}
-
 void execute_input(Instruction *instruction, JobNode jobs[4])
 {
     int alive_jobs = check_alive_jobs(jobs);
@@ -382,7 +312,6 @@ void execute_input(Instruction *instruction, JobNode jobs[4])
         }
         else
         {
-            // int exec_status = ext_com_exec(instruction, jobs);
             external_command_execute(instruction, jobs);
         }
     }
