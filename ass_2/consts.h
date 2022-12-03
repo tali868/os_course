0eead8097871d38eff8ec28108e85ff5f8d550c4
Ubuntu <ubuntu@ip-172-31-13-215.us-west-2.compute.internal>
2022-12-03 17:34:17 +0000

    opening files

--- ass_2/consts.h
+++ ass_2/consts.h
@@ -9,4 +9,6 @@ typedef int bool;
 #define MAX_FILE_COUNTER 100
 #define MAX_COMMND_FILE_NAME 200
 
+#define TREAD_FILE_NAME_TEMPLATE "thread%d.txt"
+
 #endif
--- ass_2/main.c
+++ ass_2/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <errno.h>
 #include "consts.h"
 
 int main(int argc, char *argv[])
@@ -6,8 +7,23 @@ int main(int argc, char *argv[])
     int i;
     int num_threads = atoi(argv[2]);
     int num_counters = atoi(argv[3]);
+    char file_num_name[13];
     FILE* commands_file;
-    FILE *fptr;
+    FILE* count_files[MAX_FILE_COUNTER];
+
+    for (int i=0; i<num_counters; i++)
+    {
+        snprintf(file_num_name, 13, TREAD_FILE_NAME_TEMPLATE, i);
+        count_files[i] = fopen(file_num_name, "w");
+        if (count_files[i] == NULL)
+	    {
+            printf("%s failed, errno is %d\n", "waitpid", errno);
+		    printf("Counter file isn't created. Please check and run again.");
+		    exit(1);
+	    }
+        fprintf(count_files[i], "0");
+        fflush(count_files[i]);
+    }
 
     commands_file = fopen(argv[1], "r");
 	
@@ -19,6 +35,10 @@ int main(int argc, char *argv[])
 	}
 
     fclose(commands_file);
+    for (int i=0; i<num_counters; i++)
+    {
+        fclose(count_files[i]);
+    }
 
     exit(0);
 }
